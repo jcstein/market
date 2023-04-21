08b55739956e582a9ce6e423efcb4ee9aa732dc5
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-04-20 21:54:24 -0400

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -7,11 +7,11 @@ export const NETWORK = Sepolia;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x764654d0A37e89519AD04534a90D2E183760AB59";
+export const MARKETPLACE_ADDRESS = "0xFfd9bAddF3f6e427EfAa1A4AEC99131078C1d683";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0x2bECfc6d907fD542c6f750887058bE547f598daC";
+  "0x4abebdAae7D6B9F98723eAeDd9C08A480CbFE45d";
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
