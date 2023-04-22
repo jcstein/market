722fa432bc65154bd2e2f64db71e6aa3f1d12b60
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-04-21 23:06:02 -0400

    typo and spacing

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -6,7 +6,7 @@ export default function Mint() {
     <Container maxWidth="lg">
     <h1>Mint NFTs</h1>
     <p>Connect your wallet mint an NFT from a featured collection below.</p>
-    <p>This is a will be a connection to <code>https://ipfs.thirdwebcdn.com</code>, separate from <code>https://celestia-market.vercel.app</code>.</p>
+    <p>This will be a connection to <code>https://ipfs.thirdwebcdn.com</code>, separate from <code>https://celestia-market.vercel.app</code>.</p>
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <iframe
         src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc721.html?contract=0x7e9a8Bc504ACBED631ab7a7E8216FCe0156c9a3c&chain=%7B%22name%22%3A%22Goerli%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fgoerli.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Goerli+Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22gor%22%2C%22chainId%22%3A5%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22goerli%22%7D&theme=dark"
@@ -15,6 +15,7 @@ export default function Mint() {
         frameBorder="0"
       ></iframe>
     </div>
+    <br/>
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <iframe
         src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc721.html?contract=0xD6C1f28eC5b025a618fd3733d7e6Af329a19508A&chain=%7B%22name%22%3A%22Goerli%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fgoerli.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Goerli+Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22gor%22%2C%22chainId%22%3A5%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22goerli%22%7D&theme=dark"
